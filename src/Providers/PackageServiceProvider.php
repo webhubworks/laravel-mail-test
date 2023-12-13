d4f5c6fefc0f1e27f5c913500b20ffd7f0778ff3
Johannes Ahrndt <johannes.ahrndt@rostock-ahoi.de>
2023-12-13 20:50:00 +0100

    Add provider

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -5,8 +5,18 @@ declare(strict_types=1);
 namespace WebhubWorks\MailTest\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use WebhubWorks\MailTest\Console\Commands\TestMailCommand;
 
 final class PackageServiceProvider extends ServiceProvider
 {
-
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands(
+                commands: [
+                    TestMailCommand::class,
+                ],
+            );
+        }
+    }
 }
